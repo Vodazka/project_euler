255a5741078616d08f687809c44479582648ece1
Vodazka <130486692+Vodazka@users.noreply.github.com>
2024-02-06 20:20:22 +0000

    Update 004_projecteuler.py

--- 004_projecteuler.py
+++ 004_projecteuler.py
@@ -1,7 +1,7 @@
 palindrome=0
 
-for n1 in range(100,1000) :
-    for n2 in range(100,1000) :
+for n1 in range(100,1000):
+    for n2 in range(100,1000):
         result = n1*n2
         result_str = str(result)
 
@@ -9,4 +9,4 @@ for n1 in range(100,1000) :
             if result>palindrome:
                 palindrome=result
 
-print (palindrome )               
+print (palindrome)               
